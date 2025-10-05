037672d03af835499b06cc6e4747c96aa5df96c7
rizzijp <154839703+rizzijp@users.noreply.github.com>
2025-10-05 19:05:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ elif respuesta.lower() == "y":
             # Llamo a la funcion para disparar a mi tablero
             tocado = True
             while tocado == True:
-                # Espero 3 segundos para el siguiente disparo
-                time.sleep(3)
+                # Espero 2 segundos para el siguiente disparo
+                time.sleep(2)
                 coordenada = tuple(map(int, random.randint(mi_tablero.shape[0], size = 2)))
                 print("Dispara la maquina")
                 tocado = f.recibir_disparo(mi_tablero, coordenada, mi_tablero_disparos)
@@ -112,8 +112,8 @@ elif respuesta.lower() == "y":
                     # Llamo a la funcion para disparar a mi tablero
                     tocado = True
                     while tocado == True:
-                        # Espero 3 segundos para el siguiente disparo
-                        time.sleep(3)
+                        # Espero 2 segundos para el siguiente disparo
+                        time.sleep(2)
                         coordenada = tuple(map(int, random.randint(mi_tablero.shape[0], size = 2)))
                         print("Dispara la maquina")
                         tocado = f.recibir_disparo(mi_tablero, coordenada, mi_tablero_disparos)
